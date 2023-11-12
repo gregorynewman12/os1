4b875486c196162e66e0319635d10c93593d0638
Gregory Newman <newmangr@os1.engr.oregonstate.edu>
2023-11-12 11:42:06 -0800

    Input filtering is working completely.

--- smallsh.c
+++ smallsh.c
@@ -16,6 +16,7 @@ struct arg
     struct arg *next;
 };
 
+// Creates linked list of arguments
 struct arg *createArg(char *argu)
 {
     // Pointer to hold current movie being created
@@ -40,35 +41,60 @@ int main()
     int numCharsEntered = -5;
     int currChar = -5;
     size_t bufferSize = 0;
-    int exit = 0;
-    while (!exit)
+    int running = 1;
+    while (running)
     {
         printf(": ");
         numCharsEntered = getline(&lineEntered, &bufferSize, stdin);
         lineEntered[numCharsEntered - 1] = '\0';
 
-        int counter = 0;         // Counter variable to count tokens
-        char command[100];       // Holds command name
+        int counter = 0;    // Counter variable to count tokens entered
+        char command[2048]; // Holds command name
+        memset(command, '\0', 2048);
         struct arg *head = NULL; // Head of the linked list to hold arguments
         struct arg *tail = NULL; // Tail of the linked list to hold arguments
         int hasInputFile = 0;    // Tracks whether an input file was provided
-        char inputFile[100];     // Name of the input file, if any
-        int hasOutputFile = 0;   // Tracks whether an output file was provided
-        char outputFile[100];    // Name of the output file, if any
+        char inputFile[2048];    // Name of the input file, if any
+        memset(inputFile, '\0', 2048);
+        int hasOutputFile = 0; // Tracks whether an output file was provided
+        char outputFile[2048]; // Name of the output file, if any
+        memset(outputFile, '\0', 2048);
+        int numOfArguments = 0;  // Tracks number of arguments entered
         int runInBackground = 0; // Tracks if '&' was entered
+        int noMoreArguments = 0; // Ensures arguments can't be entered after input or
+                                 // output file is specified so command is in the right order
+        int validCommand = 1;    // Assumes a command is valid. Will be set to false if certain
+                                 // conditions are not met.
+        int isCommentOrEmpty = 0;
 
-        // This section separates the input into the needed information (command, args, etc)
+        /*
+        This section separates the input into the needed information (command, args, etc)
+        */
+        if (lineEntered[0] == ' ' || lineEntered[0] == '#')
+        {
+            isCommentOrEmpty = 1;
+        }
         char *token = strtok(lineEntered, " ");
         while (token != NULL)
         {
-            // If the counter is 0 and isn't a newline or comment, the token is the command
-            if (counter == 0)
+            // If this is the first token, it is the command. Also makes sure an input, output, or '&'
+            // can't be entered before a command is specified.
+            if (counter == 0 && token[0] != '<' && token[0] != '>' && token[0] != '&')
             {
                 strcpy(command, token);
                 counter++;
             }
 
-            else if (counter != 0 && token[0] != '<' && token[0] != '>')
+            else if (counter == 0 && (token[0] == '<' || token[0] == '>' || (token[0] == '&' && strlen(token) == 1)))
+            {
+                validCommand = 0;
+                counter++;
+            }
+
+            // If criteria here is met, the token is an argument
+            // Counter can't be 0, first char isn't < or >, first char can be & only if the string length is more than 1,
+            // and the program isn't done taking arguments
+            else if (counter != 0 && token[0] != '<' && token[0] != '>' && !(token[0] == '&' && strlen(token) == 1) && noMoreArguments == 0 && runInBackground == 0)
             {
                 struct arg *newNode = createArg(token);
                 // Is this the first node in the linked list?
@@ -86,44 +112,93 @@ int main()
                     tail->next = newNode;
                     tail = newNode;
                 }
+                numOfArguments++;
             }
 
-            // Input file will be provided
-            else if (token[0] == '<')
+            // Prevents any more arguments once an input, output, or "&" has been specified.
+            else if (counter != 0 && token[0] != '<' && token[0] != '>' && (noMoreArguments == 1 || runInBackground == 1))
+            {
+                validCommand = 0;
+            }
+
+            // Input file will be provided, if one has not already been specified
+            else if (token[0] == '<' && hasInputFile == 0 && runInBackground == 0)
             {
                 // Gets next token, which will be the input file name
                 token = strtok(NULL, " ");
-                strcpy(inputFile, token);
-                hasInputFile = 1;
+
+                // Now will make sure an input file is properly specified
+                if (token == NULL || (token[0] == '&' && strlen(token) == 1) || (token[0] == '<' && strlen(token) == 1) || (token[0] == '>' && strlen(token) == 1))
+                {
+                    validCommand = 0;
+                }
+                else
+                {
+                    strcpy(inputFile, token);
+                    hasInputFile = 1;
+                    noMoreArguments = 1; // Ensures no more arguments can be entered now
+                }
             }
 
-            else if (token[0] == '>')
+            // If an input file has already been provided and user tries to provide
+            // a second, sets validCommand to 0.
+            else if (token[0] == '<' && hasInputFile == 1)
+            {
+                validCommand = 0;
+            }
+
+            // Output redirection specified, and does not already have an output file, and user hasn't already entered '&'
+            else if (token[0] == '>' && hasOutputFile == 0 && runInBackground == 0)
             {
                 // Gets next token, which will be the output file name
                 token = strtok(NULL, " ");
-                strcpy(outputFile, token);
-                hasOutputFile = 1;
+
+                // Now will make sure an output file is properly specified
+                if (token == NULL || (token[0] == '&' && strlen(token) == 1) || (token[0] == '<' && strlen(token) == 1) || (token[0] == '>' && strlen(token) == 1))
+                {
+                    validCommand = 0;
+                }
+                else
+                {
+                    strcpy(outputFile, token);
+                    hasOutputFile = 1;
+                    noMoreArguments = 1; // Ensures no more arguments can be entered now
+                }
+            }
+
+            // Output file already provided, command will be invalidated
+            else if (token[0] == '>' && hasOutputFile == 1)
+            {
+                validCommand = 0;
             }
 
             else if (token[0] == '&')
             {
                 runInBackground = 1;
+                noMoreArguments = 1;
             }
 
             token = strtok(NULL, " "); // Get the next token
         }
 
         struct arg *argsList = head;
-        if (command == NULL)
+
+        /*
+        This section reports on the user's input
+
+        if (isCommentOrEmpty)
         {
-            printf("No command was entered (comment or empty)\n");
+            printf("No command was entered (comment or empty)\n", command[0]);
+        }
+        else if (!validCommand)
+        {
+            printf("Invalid command.\n");
         }
         else
         {
             printf("Command name: %s\n", command);
-            printf("Arguments:\n");
             // Prints arguments, if any
-            if (argsList == NULL)
+            if (numOfArguments == 0)
             {
                 printf("There were no arguments entered.\n");
             }
@@ -158,9 +233,104 @@ int main()
                 printf("No output file was provided.\n");
             }
 
-            free(lineEntered);
-            lineEntered = NULL;
+            if (runInBackground)
+            {
+                printf("Run in background: True\n");
+            }
+            else
+            {
+                printf("Run in background: False\n");
+            }
+
         }
-        return 0;
+        */
+        free(lineEntered);
+        lineEntered = NULL;
+
+        /*
+        Running Commands section
+        */
+        // Only moves on if the command was valid
+
+        if (!validCommand)
+        {
+            printf("Invalid command.\n");
+        }
+        else
+        {
+            char *cd = "cd";
+            char *exit = "exit";
+            char *status = "status";
+
+            // Command is cd
+            if (strcmp(command, cd) == 0)
+            {
+                // First makes sure it has no input or output file
+                if (hasInputFile || hasOutputFile)
+                {
+                    printf("Error. Redirection unsupported.\n");
+                }
+
+                // 0 arguments (will change directory to $HOME)
+                else if (numOfArguments == 0)
+                {
+                    printf("Changing directory to $HOME.\n");
+                }
+                // 1 argument (will attempt to change directory to specified path)
+                else if (numOfArguments == 1)
+                {
+                    printf("Changing directory to specified path.\n");
+                }
+                // Invalid number of arguments
+                else
+                {
+                    printf("Incorrect arguments entered.\n");
+                }
+            }
+
+            // Command is status
+            else if (strcmp(command, status) == 0)
+            {
+                if (hasInputFile || hasOutputFile)
+                {
+                    printf("Error. Redirection unsupported.\n");
+                }
+
+                else if (numOfArguments != 0)
+                {
+                    printf("Error. Status command takes no arguments.\n");
+                }
+                else
+                {
+                    printf("\nExecuting the status command.\n\n");
+                }
+            }
+
+            // Command is exit
+            else if (strcmp(command, exit) == 0)
+            {
+                if (hasInputFile || hasOutputFile)
+                {
+                    printf("Error. Redirection unsupported.\n");
+                }
+
+                else if (numOfArguments != 0)
+                {
+                    printf("Error. Exit command takes no arguments.\n");
+                }
+                else
+                {
+                    printf("\nExecuting the exit command.\n\n");
+                    running = 0;
+                }
+            }
+
+            // Command is some other command
+            else
+            {
+                printf("External command. Attempting to run.\n");
+            }
+        }
     }
+    return 0;
 }
